e0ee453e8f7c3a6ed233a1a51ba1c562b15b1a0b
kim <kim_man@live.com>
2021-10-10 15:47:26 +1100

    dsf

--- mutations/checkout.ts
+++ mutations/checkout.ts
@@ -1,9 +1,13 @@
-/* eslint-disable */
+import { CartItem } from '../schemas/CartItem';
+import { User } from '../schemas/User';
+import {
+  CartItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
 
-import { KeystoneContext } from "@keystone-next/types";
-import { CartItemCreateInput } from "../.keystone/schema-types";
-import stripeConfig from "../lib/stripe";
-import { Session } from "../types";
+/* eslint-disable */
+import { KeystoneContext, SessionStore } from '@keystone-next/types';
+import stripeConfig from '../lib/stripe';
 
 const graphql = String.raw;
 
@@ -16,42 +20,39 @@ async function checkout(
   { token }: Arguments,
   context: KeystoneContext
 ): Promise<OrderCreateInput> {
-  // 1. make sure theyre signed in
+  // 1. Make sure they are signed in
   const userId = context.session.itemId;
   if (!userId) {
-    throw new Error("Sorry! you must be signed in to create an order!");
+    throw new Error('Sorry! You must be signed in to create an order!');
   }
-  // 1.5. query the current user
+  // 1.5 Query the current user
   const user = await context.lists.User.findOne({
     where: { id: userId },
     resolveFields: graphql`
-        name
+      id
+      name
+      email
+      cart {
         id
-        email
-        cart{
+        quantity
+        product {
+          name
+          price
+          description
+          id
+          photo {
             id
-            quantity
-            product{
-                name
-                price
-                description
-                id
-                photo{
-                    id
-                    image{
-                        id
-                        publicUrlTransformed
-                    }
-                }
+            image {
+              id
+              publicUrlTransformed
             }
+          }
         }
+      }
     `,
   });
-  console.log("🤶");
-
   console.dir(user, { depth: null });
-
-  // 2. calculate total price for their order
+  // 2. calc the total price for their order
   const cartItems = user.cart.filter((cartItem) => cartItem.product);
   const amount = cartItems.reduce(function (
     tally: number,
@@ -61,12 +62,11 @@ async function checkout(
   },
   0);
   console.log(amount);
-
   // 3. create the charge with the stripe library
   const charge = await stripeConfig.paymentIntents
     .create({
       amount,
-      currency: "USD",
+      currency: 'USD',
       confirm: true,
       payment_method: token,
     })
@@ -75,9 +75,35 @@ async function checkout(
       throw new Error(err.message);
     });
   console.log(charge);
-
-  // 4. convert the cartItems to orderItems
-  // 5. create the order and return it
+  // 4. Convert the cartItems to OrderItems
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+    return orderItem;
+  });
+  console.log('gonna create the order');
+  // 5. Create the order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+    resolveFields: false,
+  });
+  // 6. Clean up any old cart item
+  const cartItemIds = user.cart.map((cartItem) => cartItem.id);
+  console.log('gonna create delete cartItems');
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  });
+  return order;
 }
 
 export default checkout;
